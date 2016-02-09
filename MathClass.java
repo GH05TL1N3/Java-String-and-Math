9c63b7c5d214617ad3711948c04ce518b8b4ff1f
N4N0-GH05TL1N3 <gh0stsync.rt@gmail.com>
2016-02-09 16:34:38 +0700

    Create MathClass.java

---
+++ MathClass.java
@@ -0,0 +1,31 @@
+public class  MathClass{
+	public static void main(String[] args) {
+		double power_test = 0;
+		power_test = Math.pow(5,2);
+		System.out.println(power_test);
+
+		double num1 = 3.5594;
+		double num2 = 6.3271;
+		double sum;
+		sum = num1 + num2;
+		System.out.println(sum);
+
+		System.out.println(Math.round(3.27));
+		System.out.println(Math.round(3.67));
+
+		System.out.println(Math.min(10,50));
+		System.out.println(Math.max(10,50));
+
+		System.out.println(Math.ceil(sum));
+		System.out.println(Math.floor(sum));
+
+
+
+
+
+
+
+
+
+	}
+}
